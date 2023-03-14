c7badd1f4310da8e6f2c7c052821dafb734628eb
Suryansh <suryanshgrover1999@gmail.com>
2023-03-14 13:08:55 +0530

    made some corrections

--- in10__Rfam Dataset SQL Lookup.sql
+++ in10__Rfam Dataset SQL Lookup.sql
@@ -23,7 +23,7 @@ WHERE TABLE_SCHEMA="Rfam" AND (COLUMN_KEY="PRI" OR COLUMN_KEY="MUL");
 
 # c. Which type of rice has the longest DNA sequence? (hint: use the rfamseq and the taxonomy tables)
 -- Biological name of "Rice" = "Oryza sativa"
-SELECT taxo.species 
+SELECT taxo.species, MAX(seq.length)
 FROM rfamseq AS seq
 INNER JOIN 
 (
@@ -32,8 +32,21 @@ INNER JOIN
 ON seq.ncbi_id = taxo.ncbi_id 
 GROUP BY taxo.species 
 ORDER BY MAX(seq.length) DESC LIMIT 1;
+-- Oryza sativa Indica Group	47244934
 
 # OR
+
+SELECT t.species, r.length 
+FROM rfamseq AS r
+INNER JOIN 
+(
+	SELECT ncbi_id, species FROM taxonomy WHERE species LIKE "%Oryza sativa%"
+) AS t
+ON r.ncbi_id = t.ncbi_id 
+ORDER BY r.length DESC LIMIT 1;
+-- Oryza sativa Indica Group	47244934
+
+# WHEREAS the following query doesnt make any fucking sense
 
 SELECT taxo.species 
 FROM rfamseq AS seq
@@ -43,19 +56,23 @@ INNER JOIN
 ) AS taxo
 ON seq.ncbi_id = taxo.ncbi_id
 ORDER BY MAX(seq.length);
+-- Of course it's gonna give wrong results!
 
 
 # d. We want to paginate a list of the family names and their longest DNA sequence lengths (in descending order of length) 
 -- where only families that have DNA sequence lengths greater than 1,000,000 are included. Give a query that will return 
 -- the 9th page when there are 15 results per page. 
 -- (Hint: We need the family accession ID, family name and the maximum length in the results.)
-SELECT fam.rfam_acc family_accession_id, fam.description family_name, seq.length longest_dna_seq_length
+SELECT 
+	fam.rfam_acc family_accession_id, 
+	fam.description family_name, 
+	MAX(seq.length) longest_dna_seq_length
 FROM family AS fam
 INNER JOIN rfamseq AS seq
 ON fam.auto_wiki = seq.ncbi_id
 WHERE seq.length >= 1000000
 GROUP BY family_name
 ORDER BY MAX(seq.length) DESC
-LIMIT 121, 135;  # Most probably there are not as many records so as to reach the 9th page! 
-
+LIMIT 120, 135;  # Most probably there are not as many records so as to reach the 9th page! 
+-- Offsetting (skipping) the first 120 rows.
 
